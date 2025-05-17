269c84c547247592c087e017231676ea4d98e845
Yuri Matos Rodrigues <yurimatos.tuti@gmail.com>
2025-05-17 20:36:11 -0300

    feat: ajusta biblioteca

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
 
 import 'package:record/record.dart';
 
@@ -34,10 +38,94 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final AudioRecorder audioRecorder = AudioRecorder();
+  final AudioPlayer audioPlayer = AudioPlayer();
+
+  String? recordingPath;
+  bool isRecording = false, isPlaying = false;
+
+  Widget _recordingButton(){
+    return FloatingActionButton(
+      onPressed: () async {
+
+        if(isRecording){
+          String? filePath = await audioRecorder.stop();
+
+          if(filePath != null){
+            setState(() {
+              isRecording = false;
+              recordingPath = filePath;
+            });
+          }
+        }
+        else{
+          if(await audioRecorder.hasPermission()){
+            final Directory appDocumentsDir = await getApplicationCacheDirectory();
+
+            final String filePath = p.join(appDocumentsDir.path, 'recording.wav');
+
+            await audioRecorder.start(
+              const RecordConfig(), 
+              path: filePath,
+            );
+
+            setState(() {
+              isRecording = true;
+              recordingPath = null;
+            });
+          }
+        }
+      }, 
+      child: Icon(
+        isRecording ? Icons.stop : Icons.mic
+      ),
+    );
+  }
+
+  Widget _buildUI(){
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          if(recordingPath != null) 
+            MaterialButton(
+              onPressed: () async {
+                if(audioPlayer.playing){
+                  audioPlayer.stop();
+
+                  setState(() {
+                    isPlaying = false;
+                  });
+                }
+                else{
+                  await audioPlayer.setFilePath(recordingPath!);
+                  audioPlayer.play();
+                  setState(() {
+                    isPlaying = true;
+                  });
+                }
+
+              },
+              color: Theme.of(context).colorScheme.primary,
+              child: Text(isPlaying ? "Stop Playing Recording" : "Start Playing Recording"),
+            ),
+
+          if(recordingPath == null)
+            const Text("No Recording Found."),
+          
+        ],
+      )
+    );
+  }
   
   @override
   Widget build(BuildContext context) {
     
-    return Scaffold();
+    return Scaffold(
+      floatingActionButton: _recordingButton(),
+      body: _buildUI(),
+    );
   }
 }
