bb95f3760cec33782a0c990399b0ec196320bb58
Yuri Matos Rodrigues <yurimatos.tuti@gmail.com>
2025-05-29 21:00:03 -0300

    feat: cria pop-up menu

--- lib/list_audio.dart
+++ lib/list_audio.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:record_audio/menu_widget.dart';
 
 class ListAudio extends StatefulWidget {
   @override
@@ -11,14 +12,31 @@ class _ListAudioState extends State<ListAudio> {
     return SizedBox(
       height: MediaQuery.of(context).size.height * 0.7,
       child: Container(
-        color: Colors.blue,
         child: ListView(
-          padding: EdgeInsets.fromLTRB(10, 30, 10, 10),
+          padding: EdgeInsets.fromLTRB(10, 50, 10, 10),
           children: <Widget>[
             Container(
-              height: 35,
-              color: Colors.amber[100],
-              child: Text('Teste'),
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    color: Color.fromARGB(255, 14, 135, 230),
+                    width: 1,
+                  ),
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.play_arrow, size: 45),
+                    onPressed: () => {},
+                  ),
+                  Expanded(
+                    child: Text('Audio 1', style: TextStyle(fontSize: 25)),
+                  ),
+                  Menu(),
+                ],
+              ),
             ),
           ],
         ),
